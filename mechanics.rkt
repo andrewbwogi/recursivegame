ba7a813d43d3c3fa918cc7ccb50a0d743f7fdead
andrewbwogi <bwogi.andrew@gmail.com>
2018-09-02 20:05:00 +0200

    add tests to update-obstacles

--- mechanics.rkt
+++ mechanics.rkt
@@ -50,14 +50,14 @@
                           (/ (box-side-length box) 2))))
 (module+ test
   (define half-x-screen (/ FRAME-X 2))
-  (define test-box (box 0 (point half-x-screen 20) 40 (frame 40 0 0) 20 10))
-  (check-equal? (point half-x-screen 20) (update-box-center-point test-box)))
+  (define test-box (box 0 (point half-x-screen 20) 40 (frame 40 0 15) 20 10))
+  (check-equal? (update-box-center-point test-box) (point half-x-screen 20)))
 
 ; update the size of the box
 (define (update-box-side-length box)
   (+ (box-side-length box) BOX-GROWTH))
 (module+ test
-  (check-equal? (+ 40 BOX-GROWTH) (update-box-side-length test-box)))
+  (check-equal? (update-box-side-length test-box) (+ 40 BOX-GROWTH)))
 
 ; set the area that defines the borders of the box
 (define (update-box-frame box old-world-frame)
@@ -70,7 +70,8 @@
    ))
 (module+ test
   (define old-world-frame (frame 900 0 900))
-  (check-equal? (frame 880 (- half-x-screen 20) (+ half-x-screen 20)) (update-box-frame test-box old-world-frame)))
+  (check-equal? (update-box-frame test-box old-world-frame)
+                (frame 880 (- half-x-screen 20) (+ half-x-screen 20))))
 
 ; set new jump value, which is the y distance to the world bottom
 (define (update-box-jump-value old-box)  
@@ -81,8 +82,8 @@
 (module+ test
   (define new-jump-value (+ 20 (+ (* 10 TIME) (* TIME TIME (/ 1 2) GRAVITY))))
   (define test-box-ground (struct-copy box test-box [jump-value -1]))
-  (check-equal? new-jump-value (update-box-jump-value test-box))
-  (check-equal? 0 (update-box-jump-value test-box-ground)))
+  (check-equal? (update-box-jump-value test-box) new-jump-value)
+  (check-equal? (update-box-jump-value test-box-ground) 0))
 
 ; set new box velocity
 (define (update-box-velocity old-box)
@@ -91,19 +92,37 @@
       (+ (box-velocity old-box) (* GRAVITY TIME))))
 (module+ test
   (define new-velocity (+ 10 (* GRAVITY TIME)))
-  (check-equal? new-velocity (update-box-velocity test-box))
-  (check-equal? 0 (update-box-velocity test-box-ground)))
+  (check-equal? (update-box-velocity test-box) new-velocity)
+  (check-equal? (update-box-velocity test-box-ground) 0))
 
 ; make each obstacle move one step to the left
 (define (update-obstacles box obstacles frame timers)
   (append (map (lambda (o)
+                 (define new-x-value (- (obstacle-x-value o) (obstacle-velocity o)))
                  (struct-copy obstacle o
-                              [x-value (- (obstacle-x-value o) (obstacle-velocity o))]
-                              [front-edge (- (obstacle-x-value o) (/ (obstacle-width o) 2))]
-                              [rear-edge (+ (obstacle-x-value o) (/ (obstacle-width o) 2))]
+                              [x-value new-x-value]
+                              [front-edge (- new-x-value (/ (obstacle-width o) 2))]
+                              [rear-edge (+ new-x-value (/ (obstacle-width o) 2))]
                               [velocity (update-obstacle-velocity box o)]))
                (remove-gone-obstacles obstacles frame))
           (spawn-obstacle timers box frame)))
+(module+ test
+  (define empty '())
+  (define test-obstacle (list (obstacle 896.0 895.0 900.0 5 35 1.5 45)))
+  (define test-obstacles (list (obstacle 693.5 692.5 697.5 5 35 1.5 45) (obstacle 896.0 895.0 900.0 5 35 1.5 45) ))
+  (define test-gone-obstacle (list (obstacle -1 -2 -3 5 35 1.5 45) (obstacle 896.0 895.0 900.0 5 35 1.5 45)))
+  (define test-frame (frame 979 0 980))
+  (define test-timers (timers 354 13 112))
+  (define test-timers-spawn (timers -1 -1 112))
+  (check-equal? (update-obstacles test-box empty test-frame test-timers) '())
+  (check-equal? (update-obstacles test-box test-obstacle test-frame test-timers)
+                (list (obstacle 894.5 892.0 897.0 5 35 1.5 45)))
+  (check-equal? (update-obstacles test-box test-obstacles test-frame test-timers)
+                (list (obstacle 692.0 689.5 694.5 5 35 1.5 45) (obstacle 894.5 892.0 897.0 5 35 1.5 45)))
+  (check-equal? (update-obstacles test-box test-gone-obstacle test-frame test-timers)
+                (list (obstacle 894.5 892.0 897.0 5 35 1.5 45)))
+  (check-equal? (update-obstacles test-box test-obstacle test-frame test-timers-spawn)
+                (list (obstacle 894.5 892.0 897.0 5 35 1.5 45) (initialize-obstacle test-frame))))
 
 ; add a new obstacle to the world's obstacle list if it is time and the box size is not too big
 (define (spawn-obstacle timers box frame)
@@ -113,6 +132,10 @@
         (< (box-side-length box) BOX-SIZE-LIMIT-FOR-OBSTACLE-SPAWN))
    (list (initialize-obstacle frame))
    '()))
+(module+ test
+  (check-equal? (spawn-obstacle test-timers-spawn test-box test-frame)
+                (list (initialize-obstacle test-frame)))
+  (check-equal? (spawn-obstacle test-timers test-box test-frame) '()))
 
 ; accelerate the obstacle if it is under the box
 (define (remove-gone-obstacles obstacles frame)
@@ -121,6 +144,10 @@
           (rest obstacles)
           obstacles)
       '()))
+(module+ test
+  (check-equal? (remove-gone-obstacles test-gone-obstacle test-frame) (rest test-gone-obstacle))
+  (check-equal? (remove-gone-obstacles test-obstacle test-frame) test-obstacle)
+  (check-equal? (remove-gone-obstacles empty test-frame) '()))
 
 ; accelerate the obstacle if it is under the box
 (define (update-obstacle-velocity b o)
@@ -128,6 +155,10 @@
            (> (frame-right (box-frame b)) (obstacle-front-edge o)))
       (+ (obstacle-velocity o) (* (obstacle-acceleration o) TIME))
       (obstacle-velocity o)))
+(module+ test
+  (define test-obstacle-under (obstacle 7 5 9 5 35 1.5 45))
+  (check-equal? (update-obstacle-velocity test-box (first test-obstacle)) 1.5)
+  (check-equal? (update-obstacle-velocity test-box test-obstacle-under) 2.4375))
 
 ; update all timers
 (define (update-timers old-timers)
